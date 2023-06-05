e7d53cf0dae0efbc7fa393dfbd6222cffa89a9d8
Francis Alcantara <francisalcantara001@gmail.com>
2023-06-05 09:50:19 -0400

    Add logger abstraction

---
+++ src/Domain/Common/ILoggerManager.cs
@@ -0,0 +1,8 @@
+namespace Domain.Common;
+public interface ILoggerManager
+{
+    void LogInfo(string message);
+    void LogWarn(string message);
+    void LogError(string message);
+    void LogDebug(string message);
+}
