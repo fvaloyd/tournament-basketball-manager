d656ee5f1c2d2b9d3fd9da9de55f66d649866f96
Francis Alcantara <francisalcantara001@gmail.com>
2023-06-20 12:03:57 -0400

    Fix endpoints route

--- src/Presentation.API/Program.cs
+++ src/Presentation.API/Program.cs
@@ -10,6 +10,7 @@ using Application.Features.Managers.Commands;
 using Application.Features.Managers.Queries;
 using Application.Features.Organizers.Commands;
 using Application.Features.Organizers.Queries;
+using Presentation.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,8 @@ builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices();
 
 var app = builder.Build();
+
+app.ConfigureExceptionHandlerMiddlware();
 
 var playersGroup = app.MapGroup("/api/players");
 var managersGroup = app.MapGroup("api/managers");
@@ -50,25 +53,25 @@ managersGroup.MapPost("/", async ([FromBody] CreateManagerCommand command, ISend
     return Results.Ok(managerCreatedId);
 });
 
-managersGroup.MapPost("/id:guid}/teams", async (Guid id, string teamName, ISender sender, CancellationToken ct) =>
+managersGroup.MapPost("/{id:guid}/teams", async (Guid id, string teamName, ISender sender, CancellationToken ct) =>
 {
     var teamIdCreated = await sender.Send(new CreateTeamCommand { ManagerId = id, TeamName = teamName }, ct);
     return Results.Ok(teamIdCreated);
 });
 
-managersGroup.MapDelete("/id:guid}/teams", async (Guid id, ISender sender, CancellationToken ct) =>
+managersGroup.MapDelete("/{id:guid}/teams", async (Guid id, ISender sender, CancellationToken ct) =>
 {
     await sender.Send(new DissolveTeamCommand { ManagerId = id }, ct);
     return Results.NoContent();
 });
 
-managersGroup.MapPost("/id:guid}/teams/players/{playerId:guid}", async (Guid id, Guid playerId, ISender sender, CancellationToken ct) =>
+managersGroup.MapPost("/{id:guid}/teams/players/{playerId:guid}", async (Guid id, Guid playerId, ISender sender, CancellationToken ct) =>
 {
     await sender.Send(new DraftPlayerCommand { ManagerId = id, PlayerId = playerId }, ct);
     return Results.NoContent();
 });
 
-managersGroup.MapDelete("/id:guid}/teams/players/{playerId:guid}", async (Guid id, Guid playerId, ISender sender, CancellationToken ct) =>
+managersGroup.MapDelete("/{id:guid}/teams/players/{playerId:guid}", async (Guid id, Guid playerId, ISender sender, CancellationToken ct) =>
 {
     await sender.Send(new ReleasePlayerCommand { ManagerId = id, PlayerId = playerId }, ct);
     return Results.NoContent();
@@ -80,7 +83,7 @@ organizersGroup.MapPost("/", async ([FromBody] CreateOrganizerCommand command, I
     return Results.Ok(organizerCreatedId);
 });
 
-organizersGroup.MapPost("/id:guid}/tournaments", async (Guid id, string tournamentName, ISender sender, CancellationToken ct) =>
+organizersGroup.MapPost("/{id:guid}/tournaments", async (Guid id, string tournamentName, ISender sender, CancellationToken ct) =>
 {
     var tournamentCreatedId = await sender.Send(new CreateTournamentCommand { OrganizerId = id, TournamentName = tournamentName }, ct);
     return Results.Ok(tournamentCreatedId);
@@ -92,31 +95,31 @@ organizersGroup.MapPost("/{id:guid}/tournaments/teams/{teamId:guid}", async (Gui
     return Results.NoContent();
 });
 
-organizersGroup.MapDelete("/id:guid}/tournaments/teams/{teamId:guid}", async (Guid id, Guid teamId, ISender sender, CancellationToken ct) =>
+organizersGroup.MapDelete("/{id:guid}/tournaments/teams/{teamId:guid}", async (Guid id, Guid teamId, ISender sender, CancellationToken ct) =>
 {
     await sender.Send(new DiscardTeamCommand { TeamId = teamId, OrganizerId = id }, ct);
     return Results.NoContent();
 });
 
-organizersGroup.MapPost("/id:guid}/tournaments/matches", async (Guid id, ISender sender, CancellationToken ct) =>
+organizersGroup.MapPost("/{id:guid}/tournaments/matches", async (Guid id, ISender sender, CancellationToken ct) =>
 {
     await sender.Send(new MatchTeamsCommand { OrganizerId = id }, ct);
     return Results.NoContent();
 });
 
-organizersGroup.MapDelete("/id:guid}/tournaments", async (Guid id, ISender sender, CancellationToken ct) =>
+organizersGroup.MapDelete("/{id:guid}/tournaments", async (Guid id, ISender sender, CancellationToken ct) =>
 {
     await sender.Send(new FinishTournamentCommand { OrganizerId = id }, ct);
     return Results.NoContent();
 });
 
-organizersGroup.MapGet("/id:guid}", async (Guid id, ISender sender, CancellationToken ct) =>
+organizersGroup.MapGet("/{id:guid}", async (Guid id, ISender sender, CancellationToken ct) =>
 {
     var organizer = await sender.Send(new GetOrganizerQuery { OrganizerId = id }, ct);
     return Results.Ok(organizer);
 });
 
-organizersGroup.MapGet("/id:guid}/tournaments/matches", async (Guid id, ISender sender, CancellationToken ct) =>
+organizersGroup.MapGet("/{id:guid}/tournaments/matches", async (Guid id, ISender sender, CancellationToken ct) =>
 {
     var matches = await sender.Send(new GetTournamentMatchesQuery { OrganizerId = id }, ct);
     return Results.Ok(matches);
