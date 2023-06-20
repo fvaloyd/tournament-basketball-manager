3c963a0e61ac741b8799d601400c6c4b8802d2e6
Francis Alcantara <francisalcantara001@gmail.com>
2023-06-20 13:12:49 -0400

    Add validations properly

--- src/Application/Features/Managers/Commands/CreateManagerCommand.cs
+++ src/Application/Features/Managers/Commands/CreateManagerCommand.cs
@@ -37,19 +37,37 @@ public class CreateManagerCommandValidator : AbstractValidator<CreateManagerComm
 {
     public CreateManagerCommandValidator()
     {
-        RuleFor(c => c.ManagerPersonalInfo).NotNull().WithMessage("Invalid ManagerPersonalInfo.");
-        RuleFor(c => c.ManagerPersonalInfo.City).NotNull().WithMessage("City is required.");
-        RuleFor(c => c.ManagerPersonalInfo.Country).NotNull().WithMessage("Country is required.");
-        RuleFor(c => c.ManagerPersonalInfo.Code).NotNull().WithMessage("PostalCode is required.");
-        RuleFor(c => c.ManagerPersonalInfo.HouseNumber).NotNull().WithMessage("HouseNumber is required.");
-        RuleFor(c => c.ManagerPersonalInfo.Street).NotNull().WithMessage("Street is required.");
-        RuleFor(c => c.ManagerPersonalInfo.DateOfBirth).NotNull().WithMessage("DateOfBirth is required.");
-        RuleFor(c => c.ManagerPersonalInfo.Email).EmailAddress().WithMessage("Email should be a valid email address.");
-        RuleFor(c => c.ManagerPersonalInfo.FirstName).NotNull().NotEmpty().WithMessage("FirstName is required.")
-                                                    .MaximumLength(75).WithMessage("Max length for FirstName is 75 characters.")
-                                                    .MinimumLength(2).WithMessage("Min length for FirstName is 2 characters.");
-        RuleFor(c => c.ManagerPersonalInfo.LastName).NotNull().NotEmpty().WithMessage("LastName is required.")
-                                                    .MaximumLength(100).WithMessage("Max length for LastName is 100 characters.")
-                                                    .MinimumLength(2).WithMessage("Min length for LastName is 2 characters.");
+        RuleFor(c => c.ManagerPersonalInfo).SetValidator(new ManagerPersonalInfoValidator());
+    }
+}
+
+public class ManagerPersonalInfoValidator : AbstractValidator<ManagerPersonalInfo>
+{
+    public ManagerPersonalInfoValidator()
+    {
+        RuleFor(c => c.City)
+            .NotNull().WithMessage("City is required.");
+        RuleFor(c => c.Country)
+            .NotNull().WithMessage("Country is required.");
+        RuleFor(c => c.Code)
+            .NotNull().WithMessage("PostalCode is required.");
+        RuleFor(c => c.HouseNumber)
+            .NotNull().WithMessage("HouseNumber is required.");
+        RuleFor(c => c.Street)
+            .NotNull().WithMessage("Street is required.");
+        RuleFor(c => c.DateOfBirth)
+            .NotNull().WithMessage("DateOfBirth is required.");
+        RuleFor(c => c.Email)
+            .EmailAddress().WithMessage("Email should be a valid email address.");
+        RuleFor(c => c.FirstName)
+            .NotNull()
+            .NotEmpty().WithMessage("FirstName is required.")
+            .MaximumLength(75).WithMessage("Max length for FirstName is 75 characters.")
+            .MinimumLength(2).WithMessage("Min length for FirstName is 2 characters.");
+        RuleFor(c => c.LastName)
+            .NotNull()
+            .NotEmpty().WithMessage("LastName is required.")
+            .MaximumLength(100).WithMessage("Max length for LastName is 100 characters.")
+            .MinimumLength(2).WithMessage("Min length for LastName is 2 characters.");
     }
 }
--- src/Application/Features/Organizers/Commands/CreateOrganizerCommand.cs
+++ src/Application/Features/Organizers/Commands/CreateOrganizerCommand.cs
@@ -37,19 +37,37 @@ public class CreateOrganizerCommandValidator : AbstractValidator<CreateOrganizer
 {
     public CreateOrganizerCommandValidator()
     {
-        RuleFor(c => c.OrganizerPersonalInfo).NotNull().WithMessage("Invalid OrganizerPersonalInfo.");
-        RuleFor(c => c.OrganizerPersonalInfo.City).NotNull().WithMessage("City is required.");
-        RuleFor(c => c.OrganizerPersonalInfo.Country).NotNull().WithMessage("Country is required.");
-        RuleFor(c => c.OrganizerPersonalInfo.Code).NotNull().WithMessage("PostalCode is required.");
-        RuleFor(c => c.OrganizerPersonalInfo.HouseNumber).NotNull().WithMessage("HouseNumber is required.");
-        RuleFor(c => c.OrganizerPersonalInfo.Street).NotNull().WithMessage("Street is required.");
-        RuleFor(c => c.OrganizerPersonalInfo.DateOfBirth).NotNull().WithMessage("DateOfBirth is required.");
-        RuleFor(c => c.OrganizerPersonalInfo.Email).EmailAddress().WithMessage("Email should be a valid email address.");
-        RuleFor(c => c.OrganizerPersonalInfo.FirstName).NotNull().NotEmpty().WithMessage("FirstName is required.")
-                                                    .MaximumLength(75).WithMessage("Max length for FirstName is 75 characters.")
-                                                    .MinimumLength(2).WithMessage("Min length for FirstName is 2 characters.");
-        RuleFor(c => c.OrganizerPersonalInfo.LastName).NotNull().NotEmpty().WithMessage("LastName is required.")
-                                                    .MaximumLength(100).WithMessage("Max length for LastName is 100 characters.")
-                                                    .MinimumLength(2).WithMessage("Min length for LastName is 2 characters.");
+        RuleFor(c => c.OrganizerPersonalInfo).SetValidator(new OrganizerPersonalInfoValidator());
+    }
+}
+
+public class OrganizerPersonalInfoValidator : AbstractValidator<OrganizerPersonalInfo>
+{
+    public OrganizerPersonalInfoValidator()
+    {
+        RuleFor(c => c.City)
+            .NotNull().WithMessage("City is required.");
+        RuleFor(c => c.Country)
+            .NotNull().WithMessage("Country is required.");
+        RuleFor(c => c.Code)
+            .NotNull().WithMessage("PostalCode is required.");
+        RuleFor(c => c.HouseNumber)
+            .NotNull().WithMessage("HouseNumber is required.");
+        RuleFor(c => c.Street)
+            .NotNull().WithMessage("Street is required.");
+        RuleFor(c => c.DateOfBirth)
+            .NotNull().WithMessage("DateOfBirth is required.");
+        RuleFor(c => c.Email)
+            .EmailAddress().WithMessage("Email should be a valid email address.");
+        RuleFor(c => c.FirstName)
+            .NotNull()
+            .NotEmpty().WithMessage("FirstName is required.")
+            .MaximumLength(75).WithMessage("Max length for FirstName is 75 characters.")
+            .MinimumLength(2).WithMessage("Min length for FirstName is 2 characters.");
+        RuleFor(c => c.LastName)
+            .NotNull()
+            .NotEmpty().WithMessage("LastName is required.")
+            .MaximumLength(100).WithMessage("Max length for LastName is 100 characters.")
+            .MinimumLength(2).WithMessage("Min length for LastName is 2 characters.");
     }
 }
