8fa403ecb7773c91e70d0a2938686fca107518e2
Francis Alcantara <francisalcantara001@gmail.com>
2023-07-03 10:15:29 -0400

    Add mudblazor
    * Create a form to create a organizer

---
+++ src/Presentation.Web/Pages/Players/PlayersState.cs
@@ -0,0 +1,9 @@
+﻿using Shared;
+
+namespace Presentation.Web.Pages.Players;
+
+public class PlayersState
+{
+    public List<PlayerResponse> Players { get; set; }
+    public PlayerResponse CurrentPlayer { get; set; }
+}
