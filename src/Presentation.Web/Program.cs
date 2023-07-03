d4a8cbdf082dafcd8b93550e5d4aea66d18782df
Francis Alcantara <francisalcantara001@gmail.com>
2023-07-03 10:15:29 -0400

    Add mudblazor
    * Create a form to create a organizer

--- src/Presentation.Web/Program.cs
+++ src/Presentation.Web/Program.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using Presentation.Web;
+using Presentation.Web.Pages.Players;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddSingleton<PlayersState>();
+builder.Services.AddMudServices();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
